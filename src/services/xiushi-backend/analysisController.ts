18228813234964d6a3b3c42eb39449451468bb33
Riaduhb <1501303212@qq.com>
2024-11-17 22:26:11 +0800

    管理员统计分析功能（前端）

---
+++ src/services/xiushi-backend/analysisController.ts
@@ -0,0 +1,12 @@
+// @ts-ignore
+/* eslint-disable */
+import { request } from '@umijs/max';
+
+/** listTopInvokeInterfaceInfo GET /api/analysis/top/interface/invoke */
+export async function listTopInvokeInterfaceInfoUsingGet(options?: { [key: string]: any }) {
+  // @ts-ignore
+  return request<API.BaseResponseListInterfaceInfoVO>('/api/analysis/top/interface/invoke', {
+    method: 'GET',
+    ...(options || {}),
+  });
+}
